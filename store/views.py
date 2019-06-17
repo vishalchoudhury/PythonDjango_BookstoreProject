2e8ddd3f2c7f59fb0df32714d11bed6f3f406ada
vishal choudhury <vishal.choudhury@outlook.in>
2019-06-18 01:01:36 +0530

    Learnt about the model, migration and visualization

--- store/views.py
+++ store/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import HttpResponse
+from .models import book
 
 # Create your views here.
 def index(request):
 	return HttpResponse(request, 'template.html')
 
 def store(request):
-	return HttpResponse(request, 'store.html')
+	count = book.objects.all().count()
+	print(count)
+	context = {
+		'count' : '1'
+	}
+	return HttpResponse(request, 'store.html', context)
+	#return render(request, 'store.html', count)
 
 def chetan_bhagat(request):
 	return HttpResponse(request, 'chetan_bhagat.html')
